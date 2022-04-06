508002aba797e9e3b956d7cfb0490f4e897dfef7
YehezkielJonathan <103085961+YehezkielJonathan@users.noreply.github.com>
2022-04-06 11:27:34 +0800

    Create 20220406_input_output.cpp

---
+++ 20220406_input_output.cpp
@@ -0,0 +1,10 @@
+//header
+#include <iostream>
+using namespace std;
+int main() {//begin of function
+    string nama;
+    cout<<"masukan nama: "; cin>>nama;
+    cout<<"nama anda: "<<nama;
+    
+    
+}//end of function
