a37c942abfb897a0bd39a0038fa5a79ca8209135
YehezkielJonathan <103085961+YehezkielJonathan@users.noreply.github.com>
2022-04-13 10:02:56 +0800

    Create 20220413_logika.cpp

---
+++ 20220413_logika.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    /*
+    jika nilai dengan sisa bagi (%) = 0, bilangan genap jika bukan, bilangan ganjil
+    */
+    int nilai;
+    cout<<"masukan nilai = ";cin>>nilai;
+    if (nilai % 2 == 0){
+        cout<<"bilangan genap";
+    }
+    else {
+        cout<<"bilangan ganjil";
+    }
+}
