305b2b6ad0109430ba010c056cdc64d3863fc793
YehezkielJonathan <103085961+YehezkielJonathan@users.noreply.github.com>
2022-05-11 11:27:16 +0800

    Create 20220511_array2.cpp

---
+++ 20220511_array2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int nilai [5] [4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16},
+        {17, 18, 19, 20},
+    }; 
+    for (int i = 0; i<5; i++){
+        for (int j = 0; j<4; j++)
+        {
+            cout<<nilai[i][j]<<"  ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
